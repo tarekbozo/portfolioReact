dff3dd3e6649cfe56b276abe26227e1409f4120a
Tarek Bouzo <tarekbozo@gmail.com>
2020-10-09 09:45:24 +0200

    experience page added

--- src/pages/experience/index.js
+++ src/pages/experience/index.js
@@ -6,53 +6,25 @@ const Experience = () => {
     <>
       <section className='technologes'>
         <div className='content'>
-          <h3 className='content__subtitle'>Javascript</h3>
+          <h3 className='content__subtitle'>
+            Cybercom
+          </h3>
+          <span className='content__tag'>2016 -2017</span>
           <p className='content__text'>
-            I have a good amount of experience with JavaScript, building
-            websites, apps . I have worked with ECMA Script 2016 and have
-            professional experience with modern tooling such as Node, Webpack,
-            Babel and the package managers npm and yarn. I have worked in TDD
-            environments and have used the testing framework Jest.
+            I worked as a Frontend Developer within an agile sprint team. 
+            Websites and applications built using Sitevision CMS. 
           </p>
+          <div className="tags-wrapper">
+            <span className='content__tag'>CSS</span> <span className='content__tag'>HTML</span> <span className='content__tag'>JavaScript</span> <span className='content__tag'>Sass</span>
+          </div>
         </div>
 
-        <div className='gallery'>
-          <figure className='gallery__item--1'>
+        <div className='gallery-experience'>
+          <figure className='gallery__item--1 cybercom'>
             <img
               className='gallery__img'
-              src='assets/img/javascript.svg'
+              src='assets/img/visiteksjo.jpg'
               alt='Javascript'
-            />
-          </figure>
-          <figure className='gallery__item--2'>
-            <img
-              className='gallery__img'
-              src='assets/img/nodejs.svg'
-              alt='nodejs'
-            />
-          </figure>
-          <figure className='gallery__item--3'>
-            <img className='gallery__img' src='assets/img/npm.svg' alt='npm' />
-          </figure>
-          <figure className='gallery__item--4'>
-            <img
-              className='gallery__img'
-              src='assets/img/WebPack.svg'
-              alt='WebPack'
-            />
-          </figure>
-          <figure className='gallery__item--5'>
-            <img
-              className='gallery__img'
-              src='assets/img/Babel.svg'
-              alt='Babel'
-            />
-          </figure>
-          <figure className='gallery__item--6'>
-            <img
-              className='gallery__img'
-              src='assets/img/Jest.svg'
-              alt='Jest'
             />
           </figure>
         </div>
